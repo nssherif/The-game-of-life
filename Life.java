05232f3d186651ba3a16d269f60fe68c68fe436f
nssherif <nssherif@mun.ca>
2022-09-15 19:12:09 -0230

    feat: finalized game by adding game of life rules and updating the board over a specified time

--- Life.java
+++ Life.java
@@ -16,17 +16,16 @@ Where - n is the number of iterations that the program runs for
 import java.awt.Color;
 
 public class Life {
-    private int[][] currentGenCells;         // cell[i][j] = 1 if alive, 0 o/w
-    private boolean[][] nextGenCells;         // cell[i][j] = 1 if alive, 0 o/w
-    private Picture pic; // Grid to be drawn
-    private int magnification = 10;
-    private Color colors[] = {Color.WHITE, Color.BLACK};
-    private int pentaInitialConfig[][] = {
+    private int[][] currentGenCells;       // cell[i][j] = 1 if alive, 0 if dead
+    private Picture pic;                   // Grid to be drawn
+    private int magnification = 10;        // Magnification of cells on the board
+    private int pentaInitialConfig[][] = { // Initial configuration for Penta-decathlon Oscillator pattern
         {0,0,1,0,0,0,0,1,0,0},
         {1,1,0,1,1,1,1,0,1,1},
         {0,0,1,0,0,0,0,1,0,0},
     };
-    private int simkinInitialConfig[][] = { 
+
+    private int simkinInitialConfig[][] = { // Initial configuration for Simkin glider gun pattern
         {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
         {0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
         {0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
@@ -44,17 +43,26 @@ public class Life {
         {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
     };
 
-    public Life (int x, int y) {
-        currentGenCells = new int[x][y];
-        pic = new Picture(x * magnification, y * magnification);
+    /* 
+    Constructor
+    */
+    public Life (int x) {
+        currentGenCells = new int[x][x];
+        pic = new Picture(x * magnification, x * magnification);
     }
-
+    
+    /* 
+    Shows the board
+    */
     public void showBoard()
     {
         pic.show();
     }
 
-    // draws a cell
+    /* 
+    Param: The index of the cell (i, j), and the color for the cell to be colored.
+    Fills a cell found at index (i,j) with the color found in the parameter
+    */
     private void drawCell(int i, int j, Color color)
     {        
         for (int offsetX = 0; offsetX < magnification; offsetX++)
@@ -67,27 +75,27 @@ public class Life {
         }
     }
 
-    // // adds initial config current gen cells array
-    // private void addInitialConfig (int i, int j) {
-    //     currentGenCells[i][j] = pentaInitialConfig[k][l];        
-    // }
-
-    // draw each cell in initial board
+    /* 
+    Params: Takes in the grid size and pattern given by the user.
+    Initializes the board with the pattern centered in the middle of the grid. 
+    */
     private void initializeCells(int gridSize, String pattern)
     {     
         int iIterator = 0;
         int jIterator = 0;
+
         // Penta-decathlon Oscillator pattern
         if (pattern.equals("P")) {            
             for (int i = 0; i < gridSize; i++)
             {
                 for (int j = 0; j < gridSize; j++)
                 {
-                    if (i >= (gridSize/2)-1 && i<=(gridSize/2)+1) 
+                    // Aligning the initial configuration in the middle of the given grid
+                    if (i >= (gridSize/2)-1 && i<=(gridSize/2)+1) // Getting the vertical center of the grid
                     {
-                        if (j >= (gridSize/2) - (pentaInitialConfig[0].length/2) && j<(gridSize/2) + pentaInitialConfig[0].length/2) {
-                            currentGenCells[j][i] = pentaInitialConfig[iIterator][jIterator];
-                            System.out.println(i);
+                        if (j >= (gridSize/2) - (pentaInitialConfig[0].length/2) && j<(gridSize/2) + pentaInitialConfig[0].length/2) // Getting the horizontal edges from the middle where I can copy the initial pattern configuration
+                        {
+                            currentGenCells[j][i] = pentaInitialConfig[iIterator][jIterator]; // Copy initial pattern configuration
                             jIterator++;
                         }  
                     }
@@ -117,11 +125,12 @@ public class Life {
             {
                 for (int j = 0; j < gridSize; j++)
                 {
-                    if (i >= (gridSize/2)-7 && i<=(gridSize/2)+7) 
+                    // Aligning the initial configuration in the middle of the given grid
+                    if (i >= (gridSize/2)-7 && i<=(gridSize/2)+7) // Getting the vertical center of the grid
                     {
-                        if (j >= (gridSize/2) - (simkinInitialConfig[0].length/2) && j<(gridSize/2) + simkinInitialConfig[0].length/2) {
-                            currentGenCells[j][i] = simkinInitialConfig[iIterator][jIterator];
-                            System.out.println(i);
+                        if (j >= (gridSize/2) - (simkinInitialConfig[0].length/2) && j<(gridSize/2) + simkinInitialConfig[0].length/2) // Getting the horizontal edges from the middle where I can copy the initial pattern configuration
+                        {
+                            currentGenCells[j][i] = simkinInitialConfig[iIterator][jIterator]; // Copy initial pattern configuration
                             jIterator++;
                         }  
                     }
@@ -136,41 +145,63 @@ public class Life {
         showBoard();
     }
 
-    // draw each cell in initial board
-    private void drawInitialBoard()
+    /* 
+    Draws the board and shows it
+    */
+    private void draw()
     {     
         for (int i = 0; i < currentGenCells.length; i++)
         {
             for (int j = 0; j < currentGenCells.length; j++)
             {
-                // 0 == white
-                if (currentGenCells[i][j] == 0) {
+                if (currentGenCells[i][j] == 0) // 0 == white
+                {
                     drawCell(i, j, Color.WHITE);
                 }
-                // 1 == black
-                else {
+                else                            // 1 == black
+                {
                     drawCell(i, j, Color.BLACK);
                 }
             }
         }
-        
         showBoard();
     }
-
+    
     /* 
-    Params: Takes in the x and y coordinates of the cell
-    Brings cell to life by filling it with a black color
+    Params: Takes in the grid size given by user
+    Updates the board according to the rules of game of life
     */
-    private void reviveCell(int x, int y) {
-        pic.set(x*magnification, y*magnification, Color.WHITE);
-    } 
+    public void update(int gridSize) {
+        int[][] nextGenCells = new int[gridSize][gridSize];
 
-    /* 
-    Params: Takes in the x and y coordinates of the cell
-    Brings cell to life by filling it with a black color
-    */
-    private void killCell(int x, int y) {
-        pic.set(x, y, Color.WHITE);
+        for (int x = 0; x < gridSize; x++) {
+            for (int y = 0; y < gridSize; y++) {
+            int neighbors = 0;
+            int currentCell = currentGenCells[x][y];
+            for (int i = -1; i <= 1; i++) {
+                for (int j = -1; j <= 1; j++) 
+                {
+                    // uses most up-to-date values, avoid out-of-bounds
+                    neighbors += currentGenCells[(x+i + gridSize) % gridSize][(y+j + gridSize) % gridSize];
+                }
+            }
+            neighbors -= currentCell; // Subtract current cell
+
+            // Game of life rules 
+            if ((currentCell == 1) && (neighbors <  2)) // Any live cell with fewer than two live neighbouts, dies
+                nextGenCells[x][y] = 0;
+            else if ((currentCell == 1) && (neighbors == 2 | neighbors == 3 )) // Any live cell with two or three live neighbours, lives
+                nextGenCells[x][y] = 1;
+            else if ((currentCell == 1) && (neighbors > 3)) // Any live cell with more than three live neighbours, dies
+                nextGenCells[x][y] = 0;
+            else if ((currentCell == 0) && (neighbors == 3)) // Any dead cell with exactly three live neighbours, becomes alive 
+                nextGenCells[x][y] = 1;
+            else 
+                nextGenCells[x][y] = currentCell;
+            }
+        }
+
+        currentGenCells = nextGenCells;      
     }
 
     public static void main (String args[]) {
@@ -178,24 +209,18 @@ public class Life {
         int gridSize =  Integer.parseInt(args[1]);
         String pattern = args[2];
 
-        Life life = new Life(gridSize, gridSize);
+        Life life = new Life(gridSize);
 
         life.initializeCells(gridSize, pattern);
 
-        life.drawInitialBoard();
+        life.draw();
 
-        //life.killCell(1,1);
-
-        // switch (pattern) {
-        //     case (R):  // Random
-
-        //         break;
-        //     case (S):  // Simkin glider gun
+        for (int i = 0; i < numOfIterations; i++) {
+            try { Thread.sleep(100); } // Delay so we can see what's happening!
+            catch (Exception ex) { /* ignore */ }
             
-        //         break;
-        //     case (P):  // Penta-decathlon Oscillator
-
-        //         break;
-        // }
+            life.update(gridSize);
+            life.draw();
+        }
     }
 }
